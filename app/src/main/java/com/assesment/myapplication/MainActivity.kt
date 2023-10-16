944f1be24024ef0249143f1a746ebace3b446488
Shafi <jonyszone@gmail.com>
2023-10-16 16:56:57 +0600

    task for downloading larger file

---
+++ app/src/main/java/com/assesment/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.assesment.myapplication
+
+import android.os.Bundle
+import android.os.Environment
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
